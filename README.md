4d14fd3aa24c4fb761d1f0a8531c74007765438f
ZakSchenck <91504668+ZakSchenck@users.noreply.github.com>
2021-10-12 12:45:56 -0400

    Styling and Javascript Updates
    I added CSS styling to my page, and a Javascript array with a for statement.

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-This is a mobile web app project focusing on Javascript. I will be making a POKEDEX for generation 1 Pokemon.
-
-Created an Array with objects for 3 different Pokemon
+This is a mobile web app project focusing on Javascript. I will be making a POKEDEX for generation 1 Pokemon. When you click on a name, it returns certain information about the POKEMON
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
 let pokemonList = [
-  {name: 'Bulbasaur', height: 2.04, types: ['grass', 'poison']},
-  {name: 'Charmander', height: 2, types: 'fire'},
-  {name: 'Squirtle', height: 1.08, types: 'water'}
+  {
+   name: 'Bulbasaur ',
+   height: 2.4,
+   types: ['grass', 'poison']
+ },
+  {
+   name: ' Charmander ',
+   height: 2,
+   types: 'fire'
+ },
+  {
+   name: ' Squirtle ',
+   height: 1.8,
+   types: 'water'
+ }
 ];
+
+for (let i=0; i < pokemonList.length; i++) {
+  document.write('<p id="pokedex-style">' + pokemonList[i].name + ('\(Height: ') + pokemonList[i].height + '\)' + '</p> <br>')
+  if (pokemonList[i].height > 2) {
+    document.write('- Wow! That is big.')
+  }
+}
