28268a0b84639f6eac1ee26c3a3233002afc882f
ZakSchenck <91504668+ZakSchenck@users.noreply.github.com>
2021-10-16 19:40:55 -0400

    General edits to modal

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,74 @@ function showDetails(pokemon) {
     pokemonList.push(pokemon);
   }
 
+  let modalContainer = document.querySelector('#modal-container');
+  function showModal(title, text) {
+    modalContainer.innerHTML = '';
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+    modalContainer.classList.add('is-visible');
+  }
+
+  function showDialog(title, text) {
+  showModal(title, text);
+  let modalContainer = document.querySelector('#modal-container');
+  let modal = modalContainer.querySelector('.modal');
+
+  let confirmButton = document.createElement('button');
+  confirmButton.classList.add('modal-confirm');
+  confirmButton.innerText = 'Confirm';
+
+  let cancelButton = document.createElement('button');
+  cancelButton.classList.add('modal-cancel');
+  cancelButton.innerText = 'Cancel';
+
+  modal.appendChild(confirmButton);
+  modal.appendChild(cancelButton);
+
+  confirmButton.focus();
+}
+
+document.querySelector('#show-dialog').addEventListener('click', () => {
+  showDialog('Confirm action', 'Are you sure you want to do this?');
+});
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is the modal content!');
+  });
+
+
   function getAll() {
     return pokemonList
   }
@@ -60,7 +128,7 @@ function showDetails(pokemon) {
     return fetch(url).then(function (response) {
       return response.json();
     }).then(function (details) {
-      item.imageUrl = details.sprite.front_default,
+      item.imageUrl = details.sprites.front_default,
       item.height = details.height,
       item.types = details.types
     }).catch(function (e) {
